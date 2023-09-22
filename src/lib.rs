35e84275a984a9364ea2e8daf68f4645cb2e4042
Mister1SI <wazzup2690@gmail.com>
2023-09-21 19:55:16 -0500

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+pub mod server;
+pub mod client;
+
+pub struct Config {
+    pub server_mode: bool,
+    pub ip: String,
+    pub port: String,
+}
+
+pub fn help() {
+    let help_text = r#"
+================
+CHET 5 HELP MENU
+================
+
+usage: chet5 <mode> <ip> <port>
+
+mode:
+    -c: Client mode
+    -s: Server mode
+
+ip: The IPv4 address to host on or connect to.
+
+port: The port to host on or connect to.
+
+
+    "#;
+    println!("{}", help_text);
+}
---
+++ src/main.rs
@@ -0,0 +1,47 @@
+use chet5::client;
+use chet5::server;
+use chet5::Config;
+use std::env;
+use std::process;
+
+fn main() {
+    let mut args = env::args().into_iter();
+
+    let mut config: Config = Config {
+        server_mode: false,
+        ip: String::from(""),
+        port: String::from(""),
+    };
+
+    args.next();
+    if let Some(arg) = args.next() {
+        if arg == String::from("-s") {
+            config.server_mode = true;
+        } else if arg == String::from("-c") {
+            config.server_mode = false;
+        } else {
+            println!("Unknown argument {}.", arg);
+            process::exit(1);
+        }
+    } else {
+        chet5::help();
+        process::exit(1);
+    }
+
+    if let Some(arg) = args.next() {
+        config.ip = arg;
+    } else {
+        println!("No IP address was supplied as an argument.\nRun without arguments for help menu.");
+        process::exit(1);
+    }
+
+    if let Some(arg) = args.next() {
+
+    } else {
+        println!("No port was supplied as an argument.\nRun without arguments for help menu.");
+        process::exit(1);
+    }
+
+
+
+}
